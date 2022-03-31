8919b54f8d172a3d457c3ccca2609f692062c0b8
Alec Harvey <ruinhard@gmail.com>
2022-03-30 20:57:39 -0500

    fixed problem. site now displays very basic html handlebar

--- controllers/idkwhythisisnttheindex.js
+++ controllers/idkwhythisisnttheindex.js
@@ -2,27 +2,7 @@ const router = require('express').Router();
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-      const PostData = await Gallery.findAll({
-        include: [
-          {
-            model: Painting,
-            attributes: ['filename', 'description'],
-          },
-        ],
-      });
-  
-      const galleries = dbGalleryData.map((gallery) =>
-        gallery.get({ plain: true })
-      );
-  
-      res.render('homepage', {
-        galleries,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
+router.get('/', (req, res) => {
+      res.render('homepage');
   });
+  module.exports=router
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-
+const homeroutes = require("./idkwhythisisnttheindex")
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
-
+router.use('/',homeroutes)
 module.exports = router;
