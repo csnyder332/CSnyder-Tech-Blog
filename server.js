832fb7f9fc29eeb800a0ed5d956f346dbda87a7e
Chris Snyder <csnyder332@gmail.com>
2022-04-03 17:27:02 -0500

    port update

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(routes);
 
 sequelize.sync();
 
-app.listen(PORT, () => {
-    console.log(`Listening port ${PORT}!`);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
