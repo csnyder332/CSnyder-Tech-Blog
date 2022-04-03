4e45f43b7b716db8bc5d46f33db70ea5ac9aec5a
Chris Snyder <csnyder332@gmail.com>
2022-04-03 18:11:22 -0500

    update package json and proc

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use(routes);
 
 sequelize.sync();
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
