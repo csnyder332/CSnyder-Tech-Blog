dbaecddac6519c4590e61c931c14badeed0f0a2c
Chris Snyder <csnyder332@gmail.com>
2022-04-03 21:13:47 -0500

    changed port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
